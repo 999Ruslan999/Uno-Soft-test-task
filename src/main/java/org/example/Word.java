c3edee7da53f520133ec82686fc8f2efc04908f4
Ruslan Popov <ruslan_popov_2014@inbox.ru>
2024-06-04 20:10:24 +0300

    Initial commit

--- src/main/java/org/example/Word.java
+++ src/main/java/org/example/Word.java
@@ -2,8 +2,20 @@ package org.example;
 
 import org.apache.commons.lang3.math.NumberUtils;
 
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
 
 
 public class Word {
